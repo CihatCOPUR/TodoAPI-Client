85ed6be34ff44589b136b6fe7daa4af32ee6a6c6
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2024-04-08 14:36:33 +0300

    Finished Todo Project

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const TodoList = ({ todos, onDelete, onComplete }) => {
+  // Todos dizisini tersine çevirerek en son eklenen todo'nun ilk sırada olmasını sağlar
+  const sortedTodos = [...todos].reverse();
+
+  return (
+    <ul className="todoList" style={{ paddingRight: "32px" }}>
+      {sortedTodos.map((todo) => (
+        <Card key={todo.id} style={{ marginBottom: "10px" }}>
+          <Card.Body>
+            <Card.Title>{todo.name}</Card.Title>
+            <Card.Text>{todo.description}</Card.Text>
+            {!todo.completed && (
+              <Button
+                variant="success"
+                style={{ marginRight: "10px" }}
+                onClick={() => onComplete(todo._id)}
+              >
+                Complete
+              </Button>
+            )}
+            <Button
+              variant="danger"
+              onClick={() => onDelete(todo._id)}
+            >
+              Delete
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,47 +1,117 @@
+// components/HomePage.js
 import React, { useState, useEffect } from "react";
-import "./homePage.css";
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
+import Button from "react-bootstrap/Button";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import TodoList from "../components/TodoList";
+import './homePage.css'
 const HomePage = () => {
   const [todoData, setTodoData] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/api/todo");
-        if (!response.ok) {
-          throw new Error("Veri alınamadı");
-        }
-        const data = await response.json();
-        console.log(data);
-        setTodoData(data.data);
-      } catch (error) {
-        console.error("Veri alınamadı:", error);
-      }
-    };
-
     fetchData();
-  }, []); // Boş dependency array, sadece bileşenin monte edilmesinde bir kere çalışmasını sağlar.
+  }, []);
 
-  console.log(todoData);
+  //Fetch data from  API
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/todo");
+      if (!response.ok) {
+        throw new Error("Veri alınamadı");
+      }
+      const data = await response.json();
+      setTodoData(data.data);
+    } catch (error) {
+      console.error("Veri alınamadı:", error);
+    }
+  };
+//POST Todo
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/todo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: todoInput,
+          description: descriptionInput,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Todo eklenemedi");
+      }
+      setTodoInput("");
+      setDescriptionInput("");
+      fetchData();
+    } catch (error) {
+      console.error("Todo eklenemedi:", error);
+    }
+  };
+
+
+  // DELETE Todo 
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/todo/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Todo silinemedi");
+      }
+      fetchData();
+    } catch (error) {
+      console.error("Todo silinemedi:", error);
+    }
+  };
+
+
+  // Complete Todo ---Put
+
+  const handleComplete = async (id) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/todo/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ completed: true }),
+      });
+      if (!response.ok) {
+        throw new Error("Todo tamamlanamadı");
+      }
+      fetchData();
+    } catch (error) {
+      console.error("Tamamlama işlemi başarısız oldu:", error);
+    }
+  };
 
   return (
     <center>
-      <Form>
+      <Form onSubmit={handleSubmit} style={{width:'60%'}}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Todo</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="text"
+            placeholder="Enter todo"
+            value={todoInput}
+            onChange={(e) => setTodoInput(e.target.value)}
+          />
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+        <Form.Group className="mb-3" controlId="formBasicDescription">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Enter description"
+            value={descriptionInput}
+            onChange={(e) => setDescriptionInput(e.target.value)}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
@@ -49,20 +119,32 @@ const HomePage = () => {
       </Form>
 
       <Tabs
-        style={{ width: "40%", display: "flex", justifyContent: "center" }}
+        style={{
+          width: "40%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "50px",
+        }}
         defaultActiveKey="todo"
         id="uncontrolled-tab-example"
         className="mb-3"
       >
         <Tab eventKey="todo" title="todo" style={{ width: "40%" }}>
-          <ul className="todoList" style={{paddingRight:'32px'}}>
-          {todoData.map(todo => (
-            <li key={todo.id} style={{width:'100%', listStyleType:'none', border:'1px solid black', borderRadius:'5px', marginTop:'5px', backgroundColor:'007F73', color:'4CCD99'}}>{todo.name}</li>
-          ))}
-        </ul>
+          <TodoList
+            todos={todoData.filter((todo) => !todo.completed)}
+            onDelete={handleDelete}
+            onComplete={handleComplete}
+          />
         </Tab>
-        <Tab eventKey="end" title="end">
-          end
+        <Tab
+          eventKey="end"
+          title="end"
+          style={{ textDecoration: "line-through", width: "40%" }}
+        >
+          <TodoList
+            todos={todoData.filter((todo) => todo.completed)}
+            onDelete={handleDelete}
+          />
         </Tab>
       </Tabs>
     </center>
